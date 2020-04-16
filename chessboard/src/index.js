e0a1f8c06baff2cc2f970e09048ed540ea788dbb
Jude Battista <jbattista20@my.whitworth.edu>
2020-04-15 17:43:31 -0700

    Added a knight component. Or function. We don't know

--- chessboard/src/index.js
+++ chessboard/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-
+import Knight from './Components/Knight/Knight';
 
 ReactDOM.render(
-  <React.StrictMode>
-  </React.StrictMode>,
+  <Knight />,
   document.getElementById('root')
 );
 
