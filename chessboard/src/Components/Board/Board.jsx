466a1b36528d1ca9de79eec7f6056a9894d3e223
Jude Battista <jbattista20@my.whitworth.edu>
2020-04-15 18:34:32 -0700

    Modify Board to hold an array of squares

--- chessboard/src/Components/Board/Board.jsx
+++ chessboard/src/Components/Board/Board.jsx
@@ -2,20 +2,27 @@ import React from 'react';
 import Square from '../Square/Square';
 import Knight from '../Knight/Knight';
 
-function renderSquare(x, y, [knightX, knightY]) {
+function renderSquare(i, [knightX, knightY]) {
+    const x = i % 8;
+    const y = Math.floor(i / 8);
     const black = (x + y) % 2 === 1;
     const isKnightHere = knightX === x && knightY === y;
     const piece = isKnightHere ? <Knight /> : null;
 
-    return <Square black={black}>{piece}</Square>
+    return <div key={i} className="square-container">
+            <Square black={black}>{piece}</Square>
+        </div>
 }
 
-export default function Board(knightPosition) {
+export default function Board({knightPosition}) {
+    const squares = [];
+    for (let i = 0; i < 64; i++) {
+        squares.push(renderSquare(i, knightPosition));
+    }
+    
     return(
-        <div className="boardcontainer">
-            {renderSquare(0, 0, knightPosition)}
-            {renderSquare(1, 0, knightPosition)}
-            {renderSquare(2, 0, knightPosition)}
+        <div className="board-container">
+            {squares}
         </div>
     )
 }
