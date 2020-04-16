56dce18f01fdc29453b0dcb7b6faa817cb0a90be
Jude Battista <jbattista20@my.whitworth.edu>
2020-04-15 18:17:27 -0700

    Tried to refactor board to use a renderSquare function

--- chessboard/src/Components/Board/Board.jsx
+++ chessboard/src/Components/Board/Board.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import Square from '../Square/Square';
 import Knight from '../Knight/Knight';
 
-export default function Board() {
+function renderSquare(x, y, [knightX, knightY]) {
+    const black = (x + y) % 2 === 1;
+    const isKnightHere = knightX === x && knightY === y;
+    const piece = isKnightHere ? <Knight /> : null;
+
+    return <Square black={black}>{piece}</Square>
+}
+
+export default function Board(knightPosition) {
     return(
-        <div>
-            <Square black>
-                <Knight />
-            </Square>
+        <div className="boardcontainer">
+            {renderSquare(0, 0, knightPosition)}
+            {renderSquare(1, 0, knightPosition)}
+            {renderSquare(2, 0, knightPosition)}
         </div>
     )
 }
