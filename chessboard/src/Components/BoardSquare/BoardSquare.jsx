1426462035d81f7f69f9d5e17ae397fc5736a74c
Jude Battista <jbattista20@my.whitworth.edu>
2020-04-21 11:06:25 -0700

    Added highlighting using non-existent overlays

--- chessboard/src/Components/BoardSquare/BoardSquare.jsx
+++ chessboard/src/Components/BoardSquare/BoardSquare.jsx
@@ -4,12 +4,14 @@ import './BoardSquare.css';
 import { ItemTypes } from '../Constants/Constants';
 import { moveKnight, canMoveKnight } from '../Game/Game';
 import { useDrop } from 'react-dnd';
+import { Overlay } from 'react-overlay';
 
 function BoardSquare({x, y, children}) {
     const black = (x  + y) % 2 === 1;
     
     const [{isOver, canDrop}, drop] = useDrop({
         accept: ItemTypes.KNIGHT,
+        canDrop: () => canMoveKnight(x, y),
         drop: () => moveKnight(x, y),
         collect: mon => ({
             isOver: !!mon.isOver(),
@@ -22,10 +24,9 @@ function BoardSquare({x, y, children}) {
             <Square black={black}>
                 {children}
             </Square>
-            {isOver && (
-                <div  classname='droptarget'>
-                </div>
-            )}
+            {isOver && !canDrop && <Overlay color="red" />}
+            {!isOver && canDrop && <Overlay color="yellow" />}
+            {isOver && canDrop && <Overlay color="green" />}
         </div>
 
     );
